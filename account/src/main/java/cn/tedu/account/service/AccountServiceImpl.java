3bcde96636d2bbb3eb590af21c668c8e7589c7fb
980187223@qq.com <Syp950628>
2020-09-16 10:20:59 +0800

    account 接收事务消息，并执行本地事务

--- account/src/main/java/cn/tedu/account/service/AccountServiceImpl.java
+++ account/src/main/java/cn/tedu/account/service/AccountServiceImpl.java
@@ -1,17 +1,20 @@
 package cn.tedu.account.service;
 
 import cn.tedu.account.mapper.AccountMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import java.math.BigDecimal;
+
 @Service
 public class AccountServiceImpl implements AccountService {
-    @Autowired
+    @Resource
     private AccountMapper accountMapper;
 
+    @Transactional
     @Override
     public void decrease(Long userId, BigDecimal money) {
-        accountMapper.decrease(userId,money);
+        accountMapper.decrease(userId, money);
     }
 }
